d2a4178144f129f127f8bfc87ee8b36a5553f8af
DanielAudcent <danielaudcent@gmail.com>
2020-12-03 17:35:31 +0000

    A few minor additions
    Background image added to canvas.
    Program termination adjusted to cater for all OS.
    Rocket module created with some initial code.
    README now includes pygame version.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PRG Scratch - PyGame Project
 A repository for the pygame group project for programming.
+
+Dependancy list: Pygame (v2.0.0), 
---
+++ Rocket.py
@@ -0,0 +1,40 @@
+import pygame
+import globals
+
+#Potentially needed libaries:
+#import numpy as np
+
+#Rocket settings
+Rocket_size = 1
+position = {"X_pos": 0.5 * globals.WIDTH,
+                    "Y_pos": 0.5 * globals.HEIGHT}
+
+#Load rocket image(s)
+#Edited after source download: https://openclipart.org/detail/261323/cartoon-moon-rocket-remix-2
+Rocket_R = pygame.image.load("Images/Rockets/Rocket1.png")
+Rocket_L = pygame.image.load("Images/Rockets/Rocket2.png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
